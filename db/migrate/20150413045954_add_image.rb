a91cc58377ec16a8feabe6b3aa6edf9134c96de1
Destin Dopwell <ddopwell25@gmail.com>
2015-04-13 01:29:09 -0400

    Photo Upload
    Photo Uploading using Paperclip gem

---
+++ db/migrate/20150413045954_add_image.rb
@@ -0,0 +1,15 @@
+class AddImage < ActiveRecord::Migration
+  def up
+  	add_column :posts, :image_file_name, :string
+  	add_column :posts, :image_content_type, :string
+  	add_column :posts, :image_file_size, :integer
+  	add_column :posts, :image_updated_at, :datetime
+  end
+
+  def down
+  	remove_column :posts, :image_file_name, :string
+  	remove_column :posts, :image_content_type, :string
+  	remove_column :posts, :image_file_size, :integer
+  	remove_column :posts, :image_updated_at, :datetime
+  end
+end

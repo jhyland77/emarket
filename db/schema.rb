9ed09434433d2528bb0d824243e2def68b51a11c
Jake Hyland <jhyland77@gmail.com>
2015-04-27 16:12:08 -0400

    Removing confict issue text from files.

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(:version => 20150427151035) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.string   "status_type"
     t.string   "status"
   end
 
